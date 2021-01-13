5a4185a6005d9c46a8651da4817b58b6848ee2c3
anu-1806 <anushrimaurya@gmail.com>
2021-01-13 20:10:30 +0530

    added snake head

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import pygame
 pygame.init()
 
-screen_width = 1200
+# color
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+screen_width = 900
 screen_height = 600
+snake_x = 45
+snake_y = 55
+snake_size = 10
+
+gameWindow = pygame.display.set_mode((screen_width,screen_height))
 
 pygame.display.set_caption("Snake Game")
+pygame.display.update()
+
+exit_game = False
+game_over = False
+
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game = True
+    gameWindow.fill(white)
+    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
+    pygame.display.update()
+
+pygame.quit()
+quit()
